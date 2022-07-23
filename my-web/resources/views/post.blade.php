a6e9bed2ba5ef826b8ad87a7a5bc02e164ef6cad
Aqib Saeed <aqibsaeed570@gmail.com>
2022-07-23 21:38:08 +0500

    If else uses

--- my-web/resources/views/post.blade.php
+++ my-web/resources/views/post.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.master')
 
 @section('content')
-Hello from Post file
+<!-- Hello from Post file -->
+
+
+<!-- If Else statement use-->
+@if(12 < 25)
+You are Eligible.
+@else
+You are not Eligible..!
+@endif
+
+@endsection
+
+
+@section('scripts')
+<script>
+    @json('{{$bane}}')
+    <scripts/>
+
 @endsection
