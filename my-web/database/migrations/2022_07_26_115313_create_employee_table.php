e34de030aea08aa10e35bea93a90d89f441cb34f
Aqib Saeed <aqibsaeed570@gmail.com>
2022-07-26 17:02:57 +0500

    Create Migration in Laravel

---
+++ my-web/database/migrations/2022_07_26_115313_create_employee_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+//  Using Command Line
+// php artisan make:migration create_employee_table
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name');
+            $table->char('Phone');
+            $table->string('Adress');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+};
